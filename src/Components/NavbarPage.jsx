01f07faf8a04a385a8e858e96e9bc305aa793447
mrnitj <nithinraj982@gmail.com>
2023-06-24 14:31:04 +0530

    update

--- src/Components/NavbarPage.jsx
+++ src/Components/NavbarPage.jsx
@@ -43,7 +43,7 @@ const NavbarPage = () => {
         <div>
             <Navbar expand="lg" style={{ height: "6rem ", backgroundColor: "rgb(228, 226, 226)", zindex: "1000" }}>
                 <Container fluid style={{ padding: "0" }}>
-                    <div className="logo" style={{fontFamily: 'Josefin Sans' }}>Baby-Ku</div>
+                    <div className="logo" style={{fontFamily: 'Josefin Sans',cursor:'pointer' }} onClick={()=>Navigate('/')}>Baby-Ku</div>
                     <Navbar.Toggle aria-controls="navbarScroll" />
                     <Navbar.Collapse id="navbarScroll" style={{ backgroundColor: "rgb(228, 226, 226)" }}>
                         <Nav
